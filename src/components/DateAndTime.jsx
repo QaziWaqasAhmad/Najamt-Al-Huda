014395af6cfe89e4906916cc4f21679d91eac2b0
Waqas Ahmad Qazi <88557951+QaziWaqasAhmad@users.noreply.github.com>
2024-08-08 19:32:12 +0500

    8Aug

--- src/components/DateAndTime.jsx
+++ src/components/DateAndTime.jsx
@@ -1,4 +1,4 @@
-import  React, { useState } from "react";
+import React, { useState } from "react";
 import { DemoContainer } from "@mui/x-date-pickers/internals/demo";
 import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
 import { LocalizationProvider } from "@mui/x-date-pickers/LocalizationProvider";
@@ -13,8 +13,19 @@ export default function BasicDateTimePicker() {
 
     if (newDateTime) {
       const formattedDateTime = newDateTime.format("YYYY-MM-DD hh:mm:ss A");
-      console.log("Selected Date and Time:", formattedDateTime);
+      // console.log(formattedDateTime, 'timeeeeee');
     }
+  };
+
+  const shouldDisableDate = (date) => {
+    return date.day() === 5;
+  };
+
+  const shouldDisableTime = (timeValue, clockType) => {
+    if (clockType === "hours") {
+      return timeValue < 10 || timeValue > 22;
+    }
+    return false;
   };
 
   return (
@@ -24,6 +35,8 @@ export default function BasicDateTimePicker() {
           label="Basic date time picker"
           value={selectedDateTime}
           onChange={handleDateTimeChange}
+          shouldDisableDate={shouldDisableDate}
+          shouldDisableTime={shouldDisableTime}
         />
       </DemoContainer>
     </LocalizationProvider>
--- src/components/ProfessionalsSlider.jsx
+++ src/components/ProfessionalsSlider.jsx
@@ -9,21 +9,22 @@ import p4 from "../assets/p4.jpg";
 import p5 from "../assets/p5.jpg";
 import p6 from "../assets/p6.jpg";
 import Buttons from './Buttons';
-import StaticDateTimePickerLandscape from './DateAndTime';    
 import BasicDateTimePicker from "./DateAndTime";
-
-
+import Item from "antd/es/list/Item";
 
 const ProfessionalsSlider = () => {
   const cards = [
-    { image: p1, name: 'Sara', designation:'Hair Treatment ' },
-    { image: p2, name: 'Sara', designation:'Hair Treatment ' },
-    { image: p3, name: 'Sara', designation:'Hair Treatment ' },
-    { image: p4, name: 'Sara', designation:'Hair Treatment ' },
-    { image: p5, name: 'Sara', designation:'Hair Treatment ' },
-    { image: p6, name: 'Sara', designation:'Hair Treatment ' },
+    { id:1, image: p1, name: 'Sara', designation: 'Hair Treatment ' },
+    { id:2,image: p2, name: 'jhon', designation: 'Hair Treatment ' },
+    { id:3,image: p3, name: 'salman', designation: 'Hair Treatment ' },
+    { id:4,image: p4, name: 'waqas', designation: 'Hair Treatment ' },
+    { id:5,image: p5, name: 'noman', designation: 'Hair Treatment ' },
+    { id:6,image: p6, name: 'talha', designation: 'Hair Treatment ' },
   ];
+
   const [currentIndex, setCurrentIndex] = useState(0);
+  const [selectedCardIndex, setSelectedCardIndex] = useState(0);
+  // console.log(selectedCardIndex, "sadkdjdhdhd")
 
   const nextSlide = () => {
     setCurrentIndex((prevIndex) => (prevIndex + 1) % Math.ceil(cards.length / 3));
@@ -33,55 +34,63 @@ const ProfessionalsSlider = () => {
     setCurrentIndex((prevIndex) => (prevIndex === 0 ? Math.ceil(cards.length / 3) - 1 : prevIndex - 1));
   };
 
-  return (
-       <>
-        
-        <div className="steps-head text-center p-2 mt-4">
-      <span className='fw-bold'>Which professional do you prefer?</span>
-      </div>
-    <div className="carousel-container pt-3">
-      <button className="prev-button" onClick={prevSlide}>
-      <i class="fa-solid fa-chevron-left"></i>
-      </button>
-      <div className="carousel">
-        {cards.slice(currentIndex * 3, currentIndex * 3 + 3).map((card, index) => (
-          <div key={index} className="carousel-items">
-           <div className="team-card">
-               <div className="imagesss text-center m-auto">
-               <Avatar
-              alt="Remy Sharp"
-              src={card.image}
-              className="m-auto"
-              sx={{ width: 100, height: 100 }}
-            />
-               </div>
-                <h6 className="text-center mt-3">{card.name}</h6>
-                <small>{card.designation}</small>
-              <div className="select-team text-center pt-2">
-              <Buttons
-               name='Select'
-               className='select-pro p-0'
-               />
-              </div>
-                
-              </div>
-          </div>
-        ))}
-      </div>
-      <button className="next-button" onClick={nextSlide}>
-      <i class="fa-solid fa-chevron-right"></i>
-      </button>
-    </div>
+  const handleCardClick = (id) => {
+    // alert(id)
+    setSelectedCardIndex(id);
+  };
 
-    
-      <div className="date-picker pt-4">  
-       <div className="steps-head text-center p-2 mb-2">
-      <span className='fw-bold'>When would you like your service?</span>
+  return (
+    <>
+      <div className="steps-head text-center p-2 mt-4">
+        <span className='fw-bold'>Which professional do you prefer?</span>
       </div>
-      <BasicDateTimePicker/> 
+      <div className="carousel-container pt-3">
+        <button className="prev-button" onClick={prevSlide}>
+          <i className="fa-solid fa-chevron-left"></i>
+        </button>
+        <div className="carousel">
+          {cards.slice(currentIndex * 3, currentIndex * 3 + 3).map((card, index) => {
+            const globalIndex = currentIndex * 3 + index; 
+            return (
+              <div
+                key={index}
+                className={`carousel-items ${globalIndex === selectedCardIndex ? 'team-card-active' : ''}`}
+                onClick={() => handleCardClick(card.id)}
+              >
+                <div className="team-card p-2">
+                  <div className="imagesss text-center m-auto">
+                    <Avatar
+                      alt="Remy Sharp"
+                      src={card.image}
+                      className="m-auto"
+                      sx={{ width: 100, height: 100 }}
+                    />
+                  </div>
+                  <h6 className="text-center mt-2">{card.name}</h6>
+                  <small>{card.designation}</small>
+                  <div className="select-team text-center pt-2">
+                    {/* <Buttons
+                      name='Select'
+                      className='select-pro p-0'
+                    /> */}
+                  </div>
+                </div>
+              </div>
+            );
+          })}
+        </div>
+        <button className="next-button" onClick={nextSlide}>
+          <i className="fa-solid fa-chevron-right"></i>
+        </button>
       </div>
-     {/* </div> */}
-       </>
+
+      <div className="date-picker pt-4">
+        <div className="steps-head text-center p-2 mb-2">
+          <span className='fw-bold'>When would you like your service?</span>
+        </div>
+        <BasicDateTimePicker />
+      </div>
+    </>
   );
 };
 
